69212254dc286d6a160848fe07ba14de275f433a
AnTran <cao.an99@gmail.com>
2024-05-13 21:27:45 +0700

    Initial Commit

---
+++ Stanford-class/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stanford-class
+//
+//  Created by Trần Ân on 13/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stanford-class/Stanford_classApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_classApp.swift
+//  Stanford-class
+//
+//  Created by Trần Ân on 13/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_classApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
